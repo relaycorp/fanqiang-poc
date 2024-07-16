94ef9222b92a5aa8546cb87196d2e3a5f213436a
Gus Narea <gnarea@users.noreply.github.com>
2024-07-16 14:45:59 +0100

    Gateway: Support multiple input buffers in _Internet Checksum_ function

--- gateway/src/ip/checksum.ts
+++ gateway/src/ip/checksum.ts
@@ -1,12 +1,12 @@
 /**
- * Run the _Internet Checksum_ on the given buffer.
- *
- * @param buffer
+ * Run the _Internet Checksum_ on the given buffer(s).
  */
-export function calculateChecksum(buffer: Buffer): number {
+export function calculateChecksum(...buffers: readonly Buffer[]): number {
   let sum = 0;
-  for (let i = 0; i < buffer.length; i += 2) {
-    sum += buffer.readUInt16BE(i);
+  for (const buffer of buffers) {
+    for (let index = 0; index < buffer.length; index += 2) {
+      sum += buffer.readUInt16BE(index);
+    }
   }
   sum = (sum >> 16) + (sum & 0xffff);
   sum += sum >> 16;
