cd3545c7da4b4e56c5b1de9d20565f021c299249
Gus Narea <gnarea@users.noreply.github.com>
2024-07-26 18:46:57 +0100

    Gateway: Configure Caddy at first boot, not during image build

--- gateway/custom-image/configure-networking.sh
+++ gateway/custom-image/configure-networking.sh
@@ -3,4 +3,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+function configure_caddy() {
+    DOMAIN_NAME="$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DOMAIN_NAME)"
+    cat > /etc/caddy/Caddyfile <<EOF
+${DOMAIN_NAME} {
+    reverse_proxy localhost:8080
+}
+EOF
+    chown caddy:caddy /etc/caddy/Caddyfile
+    chmod 644 /etc/caddy/Caddyfile
+}
+
+configure_caddy
 /opt/vpn-gateway/bin/tun up
--- gateway/custom-image/provision.sh
+++ gateway/custom-image/provision.sh
@@ -47,17 +47,6 @@ function install_gateway() {
     cd -
 }
 
-function configure_caddy() {
-    DOMAIN_NAME="$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/DOMAIN_NAME)"
-    cat > /etc/caddy/Caddyfile <<EOF
-${DOMAIN_NAME} {
-    reverse_proxy localhost:8080
-}
-EOF
-    chown caddy:caddy /etc/caddy/Caddyfile
-    chmod 644 /etc/caddy/Caddyfile
-}
-
 configure_boot() {
     systemctl daemon-reload
     systemctl enable vpn-gateway.service
@@ -68,6 +57,5 @@ configure_boot() {
 # Main
 install_dependencies
 install_gateway
-configure_caddy
 uninstall_build_dependencies
 configure_boot
